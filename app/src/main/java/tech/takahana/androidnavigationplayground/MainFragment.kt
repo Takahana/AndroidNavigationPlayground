d20977583167a9820c5e3d103fa48a46da0f1141
Takahana <tagahana508@gmail.com>
2024-03-16 13:32:20 +0900

    show MainFragment

---
+++ app/src/main/java/tech/takahana/androidnavigationplayground/MainFragment.kt
@@ -0,0 +1,66 @@
+package tech.takahana.androidnavigationplayground
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.Fragment
+import tech.takahana.androidnavigationplayground.ui.theme.AndroidNavigationPlaygroundTheme
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                AndroidNavigationPlaygroundTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        MainScreen()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+internal fun MainScreen() {
+    Box(
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(text = MainFragment::class.java.simpleName)
+    }
+}
+
+@Preview
+@Composable
+internal fun MainScreenPreview() {
+    AndroidNavigationPlaygroundTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            MainScreen()
+        }
+    }
+}
