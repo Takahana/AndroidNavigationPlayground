cbba0474dc7315b28bd10d0f28089ee8ae8e443d
Takahana <tagahana508@gmail.com>
2024-03-17 14:26:27 +0900

    add SearchFragment

---
+++ features/search/src/main/java/tech/takahana/androidnavigationplayground/search/SearchFragment.kt
@@ -0,0 +1,66 @@
+package tech.takahana.androidnavigationplayground.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.Fragment
+import tech.takahana.androidnavigationplayground.uicomponent.ui.theme.AndroidNavigationPlaygroundTheme
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                AndroidNavigationPlaygroundTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        SearchScreen()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+internal fun SearchScreen() {
+    Box(
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(text = SearchFragment::class.java.simpleName)
+    }
+}
+
+@Preview
+@Composable
+internal fun SearchScreenPreview() {
+    AndroidNavigationPlaygroundTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            SearchScreen()
+        }
+    }
+}
