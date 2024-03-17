a88607362030742da74738cc6f19a692cabc1b68
Takahana <tagahana508@gmail.com>
2024-03-17 16:22:17 +0900

    add PlayerActivity

---
+++ feature/player/src/main/java/tech/takahana/androidnavigationplayground/player/PlayerActivity.kt
@@ -0,0 +1,57 @@
+package tech.takahana.androidnavigationplayground.player
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tech.takahana.androidnavigationplayground.uicomponent.ui.theme.AndroidNavigationPlaygroundTheme
+
+class PlayerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContent {
+            AndroidNavigationPlaygroundTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    PlayerScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+internal fun PlayerScreen()  {
+    Box(
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(text = PlayerActivity::class.java.simpleName)
+    }
+}
+
+@Preview
+@Composable
+internal fun PlayerScreenPreview()  {
+    AndroidNavigationPlaygroundTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            PlayerScreen()
+        }
+    }
+}
