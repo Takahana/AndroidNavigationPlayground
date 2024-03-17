7048ff4dd77928e8a2e565b2f034bddfa1e9d9f8
Takahana <tagahana508@gmail.com>
2024-03-17 17:30:04 +0900

    add TrendFragment

---
+++ feature/trend/src/main/java/tech/takahana/androidnavigationplayground/trend/TrendFragment.kt
@@ -0,0 +1,66 @@
+package tech.takahana.androidnavigationplayground.trend
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.Fragment
+import tech.takahana.androidnavigationplayground.uicomponent.ui.theme.AndroidNavigationPlaygroundTheme
+
+class TrendFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                AndroidNavigationPlaygroundTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        TrendScreen()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+internal fun TrendScreen() {
+    Box(
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(text = TrendFragment::class.java.simpleName)
+    }
+}
+
+@Preview
+@Composable
+internal fun TrendScreenPreview() {
+    AndroidNavigationPlaygroundTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            TrendScreen()
+        }
+    }
+}
