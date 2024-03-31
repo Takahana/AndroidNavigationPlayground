1aab47c2b75e683dfe5d82817a0ee8b5d68ed6fc
Takahana <tagahana508@gmail.com>
2024-03-31 20:38:31 +0900

    show card on PlayerScreen

--- feature/player/src/main/java/tech/takahana/androidnavigationplayground/player/PlayerActivity.kt
+++ feature/player/src/main/java/tech/takahana/androidnavigationplayground/player/PlayerActivity.kt
@@ -3,15 +3,26 @@ package tech.takahana.androidnavigationplayground.player
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import tech.takahana.androidnavigationplayground.uicomponent.ui.navigation.MyAppScreenDestination
 import tech.takahana.androidnavigationplayground.uicomponent.ui.theme.AndroidNavigationPlaygroundTheme
 
 class PlayerActivity : AppCompatActivity() {
@@ -32,12 +43,74 @@ class PlayerActivity : AppCompatActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun PlayerScreen()  {
-    Box(
-        contentAlignment = Alignment.Center,
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("${PlayerActivity::class.simpleName}")
+                }
+            )
+        },
+    ) { innerPadding ->
+        PlayerContents(
+            modifier = Modifier.padding(innerPadding),
+            onClick = {},
+        )
+    }
+}
+
+@Composable
+internal fun PlayerContents(
+    onClick: (MyAppScreenDestination<*>) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        contentPadding = PaddingValues(16.dp),
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        Text(text = PlayerActivity::class.java.simpleName)
+        val itemModifier = Modifier.fillMaxWidth()
+        item {
+            PlayerContent(
+                content = {
+                    Text(text = "Open Player")
+                },
+                onClick = {
+                    onClick(MyAppScreenDestination.Player)
+                },
+                modifier = itemModifier,
+            )
+        }
+        item {
+            PlayerContent(
+                content = {
+                    Text(text = "Open Trend")
+                },
+                onClick = {
+                    onClick(MyAppScreenDestination.Trend("trendId"))
+                },
+                modifier = itemModifier,
+            )
+        }
+    }
+}
+
+@Composable
+internal fun PlayerContent(
+    content: @Composable () -> Unit,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier
+            .clickable { onClick() },
+    ) {
+        Box(modifier = Modifier.padding(16.dp)) {
+            content()
+        }
     }
 }
 
