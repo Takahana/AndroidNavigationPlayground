ccd8d16ba3de65e62b5647721e0a26ebfe49e8eb
Takahana <tagahana508@gmail.com>
2024-03-31 16:34:48 +0900

    add BottomNavigationItem

---
+++ navigator/src/main/java/tech/takahana/androidnavigationplayground/navigator/BottomNavigationItem.kt
@@ -0,0 +1,19 @@
+package tech.takahana.androidnavigationplayground.navigator
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavOptions
+
+object BottomNavigationItem : ScreenLocation {
+    override fun createNavOptions(
+        navController: NavController,
+    ) = NavOptions.Builder()
+        .setLaunchSingleTop(true)
+        .setRestoreState(true)
+        .setPopUpTo(
+            route = navController.graph.findStartDestination().route,
+            inclusive = false,
+            saveState = true
+        )
+        .build()
+}
