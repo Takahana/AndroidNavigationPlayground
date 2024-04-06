3cc649ba2b2574934b667a17eb1c74169838a747
Takahana <tagahana508@gmail.com>
2024-04-06 11:14:18 +0900

    add ScreenNavigationDispatcher

---
+++ navigator/src/main/java/tech/takahana/androidnavigationplayground/navigator/ScreenNavigationDispatcher.kt
@@ -0,0 +1,46 @@
+package tech.takahana.androidnavigationplayground.navigator
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+class ScreenNavigationDispatcher(
+    private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Main + SupervisorJob()),
+) {
+
+    private val requestDeque = ArrayDeque<ScreenNavigationRequest>()
+    private val mutex = Mutex()
+
+    private val mutableScreenNavigationRequest = MutableStateFlow<ScreenNavigationRequest?>(null)
+    val screenNavigationRequest = mutableScreenNavigationRequest.asStateFlow()
+
+    fun dispatch(destination: ScreenDestination<*>) =
+        updateRequestDeque {
+            addLast(ScreenNavigationRequest(destination))
+        }
+
+    fun responded(request: ScreenNavigationRequest) =
+        updateRequestDeque {
+            remove(request)
+        }
+
+    private fun updateRequestDeque(
+        block: ArrayDeque<ScreenNavigationRequest>.() -> Unit,
+    ) {
+        coroutineScope.launch {
+            mutex.withLock {
+                requestDeque.block()
+                mutableScreenNavigationRequest.value = requestDeque.firstOrNull()
+            }
+        }
+    }
+
+    data class ScreenNavigationRequest(
+        val destination: ScreenDestination<*>,
+    )
+}
