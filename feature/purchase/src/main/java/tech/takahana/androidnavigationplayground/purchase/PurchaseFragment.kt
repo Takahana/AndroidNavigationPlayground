f1f41ae5962ef21b679b0e8c767073250327cd04
Takahana <tagahana508@gmail.com>
2024-04-21 16:01:51 +0900

    add PurchaseFragment

---
+++ feature/purchase/src/main/java/tech/takahana/androidnavigationplayground/purchase/PurchaseFragment.kt
@@ -0,0 +1,66 @@
+package tech.takahana.androidnavigationplayground.purchase
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.Fragment
+import tech.takahana.androidnavigationplayground.uicomponent.ui.theme.AndroidNavigationPlaygroundTheme
+
+class PurchaseFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                AndroidNavigationPlaygroundTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        PurchaseScreen()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+internal fun PurchaseScreen() {
+    Box(
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(text = "Purchase Screen")
+    }
+}
+
+@Preview
+@Composable
+internal fun PurchaseScreenPreview() {
+    AndroidNavigationPlaygroundTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            PurchaseScreen()
+        }
+    }
+}
