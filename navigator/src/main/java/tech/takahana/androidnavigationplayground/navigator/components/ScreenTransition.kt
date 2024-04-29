334c1a2f15ea5d59cfd6a4dff473b335115649ef
Takahana <tagahana508@gmail.com>
2024-04-29 17:48:08 +0900

    add anim properties

--- navigator/src/main/java/tech/takahana/androidnavigationplayground/navigator/components/ScreenTransition.kt
+++ navigator/src/main/java/tech/takahana/androidnavigationplayground/navigator/components/ScreenTransition.kt
@@ -1,8 +1,22 @@
 package tech.takahana.androidnavigationplayground.navigator.components
 
+import androidx.annotation.AnimRes
 import androidx.navigation.NavOptions
 
 interface ScreenTransition {
+
+    @get:AnimRes
+    val enterAnim: Int
+
+    @get:AnimRes
+    val exitAnim: Int
+
+    @get:AnimRes
+    val popEnterAnim: Int
+
+    @get:AnimRes
+    val popExitAnim: Int
+
 
     fun applyNavOptions(builder: NavOptions.Builder): NavOptions.Builder
 }
--- navigator/src/main/java/tech/takahana/androidnavigationplayground/navigator/components/transitions/Modal.kt
+++ navigator/src/main/java/tech/takahana/androidnavigationplayground/navigator/components/transitions/Modal.kt
@@ -5,9 +5,18 @@ import tech.takahana.androidnavigationplayground.navigator.R
 import tech.takahana.androidnavigationplayground.navigator.components.ScreenTransition
 
 object Modal : ScreenTransition {
+    override val enterAnim: Int
+        get() = R.anim.modal_enter_anim
+    override val exitAnim: Int
+        get() = androidx.navigation.ui.R.anim.nav_default_exit_anim
+    override val popEnterAnim: Int
+        get() = androidx.navigation.ui.R.anim.nav_default_pop_enter_anim
+    override val popExitAnim: Int
+        get() = R.anim.modal_exit_anim
+
     override fun applyNavOptions(builder: NavOptions.Builder) = builder
-        .setEnterAnim(R.anim.modal_enter_anim)
-        .setExitAnim(androidx.navigation.ui.R.anim.nav_default_exit_anim)
-        .setPopEnterAnim(androidx.navigation.ui.R.anim.nav_default_pop_enter_anim)
-        .setPopExitAnim(R.anim.modal_exit_anim)
+        .setEnterAnim(enterAnim)
+        .setExitAnim(exitAnim)
+        .setPopEnterAnim(popEnterAnim)
+        .setPopExitAnim(popExitAnim)
 }
